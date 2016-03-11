f991cc7a4fdf13efc2cd78f493c7041c850760be
John Anthony Rivera <askinganthony@gmail.com>
2016-03-11 14:09:40 -0800

    Done.

--- spec/Index.spec.js
+++ spec/Index.spec.js
@@ -3,5 +3,14 @@ var path = require('path');
 describe('angularjs homepage todo list', function() {
 	it('should add a new contact', function() {
 		browser.get('http://localhost:8080');
+    var contacts = element.all(by.repeater('contact in vm.contacts'));
+    expect(contacts.count()).toEqual(3);
+    element(by.model('contact.name')).sendKeys('A name!!');
+    element(by.model('contact.phone')).sendKeys('8675309');
+    element(by.css('[value="add"]')).click();
+
+    contacts = element.all(by.repeater('contact in vm.contacts'));
+    expect(contacts.count()).toEqual(4);
+    //expect(todoList.get(2).getText()).toEqual('Writing tests!!');
 	});
 });
