8c85943ceeb8375eb9196f6f743b212ec2c04e3a
shmulim <shmuel.markel@gmail.com>
2016-03-22 01:29:27 -0700

    Done.

--- spec/Index.spec.js
+++ spec/Index.spec.js
@@ -2,6 +2,15 @@ var path = require('path');
 
 describe('angularjs homepage todo list', function() {
 	it('should add a new contact', function() {
-		browser.get('http://localhost:8080');
+  	browser.get('http://localhost:8080');
+
+    element(by.css('.button')).click();
+
+    var contact = element.all(by.repeater('contact in vm.contacts'));
+    expect(contact.count()).toEqual(4);
+
+    element(by.model('contact.name')).sendKeys('Tim');
+    element(by.model('contact.phone')).sendKeys('3934203242');
+
 	});
 });
