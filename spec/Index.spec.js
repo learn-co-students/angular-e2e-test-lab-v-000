e46234aeca59cf210dfd1d7dc0ff007e89103d25
Justin Thomas <jthomas0209@gmail.com>
2016-03-28 10:21:07 -0400

    Done

--- spec/Index.spec.js
+++ spec/Index.spec.js
@@ -3,5 +3,14 @@ var path = require('path');
 describe('angularjs homepage todo list', function() {
 	it('should add a new contact', function() {
 		browser.get('http://localhost:8080');
+    
+    element(by.css('.button')).click();
+    
+    var todos = element.all(by.repeater('contact in vm.contacts'));
+    
+    expect(todos.count()).toEqual(4);
+    expect(todos.get(1).element(by.model('contact.name')).getAttribute('value')).toBe("Tim");
+    expect(todos.get(1).element(by.model('contact.phone')).getAttribute('value')).toBe("3934203242");
 	});
 });
+
