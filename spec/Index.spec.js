7eba2a96c7871aadf0942f33d9f3aea3efca913c
Rawradin <edgar.rawr@gmail.com>
2016-04-12 08:27:41 -0700

    Done.

--- spec/Index.spec.js
+++ spec/Index.spec.js
@@ -3,5 +3,18 @@ var path = require('path');
 describe('angularjs homepage todo list', function() {
 	it('should add a new contact', function() {
 		browser.get('http://localhost:8080');
+		
+		element(by.css('.button')).click();
+
+		var todoList = element.all(by.repeater('contact in vm.contacts'));
+		expect(todoList.count()).toEqual(4);
+
+		expect(todoList.get(1).element(by.model('contact.name')).getAttribute('value')).toBe('Tim');
+		expect(todoList.get(1).element(by.model('contact.phone')).getAttribute('value')).toBe('3934203242');
 	});
 });
+
+
+// Total count of contacts should be 4. input ng-model
+// The name of the second contact should be 'Tim'.
+// The phone number of the second contact should be '3934203242'.
