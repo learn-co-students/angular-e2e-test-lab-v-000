c762c6a8aeb68e7c8405f06738349d6f0b1b48eb
c1505 <coreym247@gmail.com>
2016-05-05 18:30:17 -0400

    Done.

--- spec/Index.spec.js
+++ spec/Index.spec.js
@@ -4,12 +4,14 @@ describe('angularjs homepage todo list', function() {
 	it('should add a new contact', function() {
 		browser.get('http://localhost:8080');
 
+    element(by.css('[class="button"]')).click();
+
     var contacts = element.all(by.repeater('contact in vm.contacts'));
 
-    expect( contacts.count() ).toEqual(4);
-    expect(contacts.get(1).getText()).toEqual('Tim');
-    expect(contacts.get(1).getText()).toEqual('3934203242');
 
+    expect( contacts.count() ).toEqual(4);
+    expect(contacts.get(1).element(by.model('contact.name')).getAttribute('value')).toBe('Tim');
+    expect(contacts.get(1).element(by.model('contact.phone')).getAttribute('value')).toBe('3934203242');
 
 	});
 });
