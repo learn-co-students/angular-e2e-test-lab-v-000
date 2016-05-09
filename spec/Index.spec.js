37173e60e1c8ea27c692380c0b039623e629edd6
febbraiod <donedesign1@gmail.com>
2016-05-09 11:17:37 -0400

    Done.

--- spec/Index.spec.js
+++ spec/Index.spec.js
@@ -1,7 +1,13 @@
 var path = require('path');
 
-describe('angularjs homepage todo list', function() {
+describe('index page', function() {
 	it('should add a new contact', function() {
 		browser.get('http://localhost:8080');
+    element(by.css('.button')).click();
+
+    var contacts = element.all(by.repeater('contact in vm.contacts'));
+    expect(contacts.count()).toEqual(4);
+    expect(contacts.get(1).name.getText()).toEqual('Tim');
+    expect(contacts.get(1).phone.getText()).toEqual('3934203242');
 	});
 });
