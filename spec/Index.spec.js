c1bdc5b685b9fbceb010619f1f353bc3425cf23d
authorbeard <msnolan@gmail.com>
2016-05-19 22:43:14 -0400

    specs passing

--- spec/Index.spec.js
+++ spec/Index.spec.js
@@ -1,7 +1,17 @@
 var path = require('path');
 
-describe('angularjs homepage todo list', function() {
+describe('clicking the add button', function() {
 	it('should add a new contact', function() {
 		browser.get('http://localhost:8080');
-	});
+
+  element(by.css('[value="add-contact"]')).click();
+  var contacts=element.all(by.repeater('contact in vm.contacts'));
+  var names=element.all(by.model('contact.name'))
+  
+
+  expect(contacts.count()).toEqual(4)
+  expect(contacts.get(1).element(by.model('contact.name')).getAttribute('value')).toBe('Tim')
+  expect(contacts.get(1).element(by.model('contact.phone')).getAttribute('value')).toBe('3934203242')
+  });
 });
+
