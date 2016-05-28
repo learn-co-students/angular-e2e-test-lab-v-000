141400f1093ee19c97444469c5d1a2833a9d528a
evanscloud <dijkstrascloud@gmail.com>
2016-05-28 12:56:02 -0400

    Edit index.spec.js test to work

--- spec/Index.spec.js
+++ spec/Index.spec.js
@@ -3,5 +3,12 @@ var path = require('path');
 describe('angularjs homepage todo list', function() {
 	it('should add a new contact', function() {
 		browser.get('http://localhost:8080');
+
+		element(by.css('.button')).click();
+
+		var contactList = element.all(by.repeater('contact in vm.contacts'));
+    expect(contactList.count()).toEqual(4);
+    expect(contactList.get(1).element(by.model('contact.name')).getAttribute('value')).toBe('Tim');
+		expect(contactList.get(1).element(by.model('contact.phone')).getAttribute('value')).toBe('3934203242');
 	});
 });
