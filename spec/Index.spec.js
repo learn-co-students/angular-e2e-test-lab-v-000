7b56fcd03c447d7abc17555a6e4fb661449e6d11
genericlady <yiannakokalas@gmail.com>
2016-06-01 18:04:10 -0400

    Done.

--- spec/Index.spec.js
+++ spec/Index.spec.js
@@ -1,7 +1,17 @@
 var path = require('path');
 
-describe('angularjs homepage todo list', function() {
+describe('angularjs homepage contact list', function() {
 	it('should add a new contact', function() {
 		browser.get('http://localhost:8080');
+
+		element(by.model('contact.name')).sendKeys('Yonk');
+		element(by.model('contact.phone')).sendKeys('Was here!!');
+
+		element(by.css('.button')).click();
+
+		var contactList = element.all(by.repeater('contact in vm.contacts'));
+
+		expect(contactList.count()).toEqual(4);
+		expect(contactList.get(1).element(by.model('contact.name')).getAttribute('value')).toBe('Tim');
 	});
 });
