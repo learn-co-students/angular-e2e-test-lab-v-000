9da82ecd2a6b81dd4bd8477c476b939a88f28e47
viparthasarathy <viparthasarathy@gmail.com>
2016-06-02 14:29:25 -0700

    Done.

--- spec/Index.spec.js
+++ spec/Index.spec.js
@@ -3,5 +3,11 @@ var path = require('path');
 describe('angularjs homepage todo list', function() {
 	it('should add a new contact', function() {
 		browser.get('http://localhost:8080');
+		var contacts = element.all(by.repeater('contact in vm.contacts'));
+		expect(contacts.get(1).element(by.model('contact.name')).getAttribute('value')).toEqual('Tim');
+		expect(contacts.get(1).element(by.model('contact.phone')).getAttribute('value')).toEqual('3934203242');
+		expect(contacts.count()).toEqual(3);
+		element(by.css('.button')).click();
+		expect(contacts.count()).toEqual(4);
 	});
 });
