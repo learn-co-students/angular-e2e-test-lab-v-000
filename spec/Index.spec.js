3e47f3d1f1121761824c10783559e9dbe021713a
reichardn <reichardn@gmail.com>
2016-08-14 14:03:01 -0400

    Done.

--- spec/Index.spec.js
+++ spec/Index.spec.js
@@ -3,5 +3,12 @@ var path = require('path');
 describe('angularjs homepage todo list', function() {
 	it('should add a new contact', function() {
 		browser.get('http://localhost:8080');
+    element(by.css('[value="add"]')).click();
+
+    var contacts = element.all(by.repeater('contact in vm.contacts'));
+    expect(contacts.count()).toEqual(4);
+    expect(contacts.get(1).getText()).toContain('Tim');
+    expect(contacts.get(1).getText()).toContain('3934203242');
 	});
 });
+
