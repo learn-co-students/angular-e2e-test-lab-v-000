8014231f1bb998f092b6314e335174b030aebcac
agdavid <antonio.g.david@gmail.com>
2016-08-30 10:13:51 -0400

    add unit test for event based directive

--- spec/Index.spec.js
+++ spec/Index.spec.js
@@ -6,7 +6,14 @@ describe('angularjs homepage todo list', function() {
     browser.get('http://localhost:8080');
 
     // click on button to add contact
+    element(by.css('[class="button"]')).click();
 
-    // set expectations
-	});
+    // set variable to repeated elements
+    var contactList = element.all(by.repeater('contact in vm.contacts'));
+
+    // set expectations of list count and values for second object in array
+    expect(contactList.count()).toEqual(4);
+    expect(contactList.get(1).element(by.model('contact.name')).getAttribute('value')).toEqual('Tim');
+    expect(contactList.get(1).element(by.model('contact.phone')).getAttribute('value')).toEqual('3934203242');
+  });
 });
