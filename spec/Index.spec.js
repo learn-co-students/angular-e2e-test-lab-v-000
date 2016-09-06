1f64ecb02dd1b587659421993b7d05dd38374b99
hudbuz <hzb2@cornell.edu>
2016-09-06 17:19:28 -0400

    Done.

--- spec/Index.spec.js
+++ spec/Index.spec.js
@@ -1,7 +1,15 @@
 var path = require('path');
 
-describe('angularjs homepage todo list', function() {
+describe('localhost add contact page', function() {
 	it('should add a new contact', function() {
-		browser.get('http://localhost:8080');
+		browser.get('http://localhost:8000');
+
+    element(by.css('[ng-click="vm.addContact()"]')).click();
+    var contacts = element.all(by.repeater('contact in vm.contacts'))
+    
+    expect(contacts.count()).toEqual(4)
+
+    expect(contacts.get(1).element(by.model('contact.name')).getAttribute('value')).toEqual('Tim')
+    expect(contacts.get(1).element(by.model('contact.phone')).getAttribute('value')).toEqual('3934203242')
 	});
 });
