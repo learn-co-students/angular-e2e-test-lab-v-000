a5ffb6e95cac9764039ac274454f4dfc3e7faa6d
wedesignstudios <deelden@gmail.com>
2016-10-11 10:32:16 -0500

    Done

--- spec/Index.spec.js
+++ spec/Index.spec.js
@@ -3,5 +3,10 @@ var path = require('path');
 describe('angularjs homepage todo list', function() {
 	it('should add a new contact', function() {
 		browser.get('http://localhost:8080');
+
+		element(by.css('.button')).click();
+
+		var todoList = element.all(by.repeater('contact in vm.contacts'));
+		expect(todoList.count()).toEqual(4);
 	});
 });
