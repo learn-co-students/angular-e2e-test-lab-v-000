b2b0deea7b61ff7c90c3c604e587a6cbc259d895
krisnotchris <krishenderson@gmail.com>
2016-10-29 20:07:04 -0500

    Done.

--- spec/Index.spec.js
+++ spec/Index.spec.js
@@ -3,5 +3,7 @@ var path = require('path');
 describe('angularjs homepage todo list', function() {
 	it('should add a new contact', function() {
 		browser.get('http://localhost:8080');
+    expect(todoList.count()).toEqual(4);
+    expect(todoList.get(2).getText()).toEqual('Writing tests!!');
 	});
 });
