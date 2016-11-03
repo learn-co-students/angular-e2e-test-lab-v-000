cc01cd182db9f25673f11c4450f0b800b87a57f2
kaileighrose <kaileighrose@gmail.com>
2016-11-02 22:23:38 -0400

    Done.

--- spec/Index.spec.js
+++ spec/Index.spec.js
@@ -3,5 +3,13 @@ var path = require('path');
 describe('angularjs homepage todo list', function() {
 	it('should add a new contact', function() {
 		browser.get('http://localhost:8080');
+
+    element(by.model('vm.newName')).sendKeys('Liz');
+    element(by.model('vm.newPhone')).sendKeys('8675309');
+    element(by.css('.button')).click();
+ 
+    var contacts = element.all(by.repeater('contact in vm.contacts'));
+        expect(contacts.count()).toEqual(4);
+        expect(contacts.get(1).getInnerHtml()).toEqual('<h4 class="ng-binding">Name: Tim</h4><h6 class="ng-binding">Phone: 3934203242</h6>');
 	});
 });
