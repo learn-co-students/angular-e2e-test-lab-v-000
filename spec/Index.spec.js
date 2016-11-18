1b9353bfccd454636ffab6889fc406461d92df4d
github username <github email address>
2016-11-18 11:52:34 -0500

    Done.

--- spec/Index.spec.js
+++ spec/Index.spec.js
@@ -1,7 +1,19 @@
 var path = require('path');
 
-describe('angularjs homepage todo list', function() {
+describe('contact homepage', function() {
 	it('should add a new contact', function() {
 		browser.get('http://localhost:8080');
 	});
 });
+
+describe('contact homepage', function() {
+	it('should add a new contact', function() {
+		browser.get('http://localhost:8080');
+
+		element(by.css('button')).click();
+		var contacts = element.all(by.repeater('contact in vm.contacts'));
+		expect (contacts.count()).toEqual(4);
+		expect(contacts.get(1).element(by.model('contact.name')).getAttribute('value')).toEqual('Tim');
+		expect(contacts.get(1).element(by.model('contact.phone')).getAttribute('value')).toEqual('3934203242');
+	})
+})
