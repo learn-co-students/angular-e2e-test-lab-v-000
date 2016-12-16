d3b757ed1b505176ce732bc99a9be88b5282bd1f
Caleb Faught <faughtcj@gmail.com>
2016-12-15 18:33:39 -0600

    Done.

--- spec/Index.spec.js
+++ spec/Index.spec.js
@@ -3,5 +3,9 @@ var path = require('path');
 describe('angularjs homepage todo list', function() {
 	it('should add a new contact', function() {
 		browser.get('http://localhost:8080');
+		element(by.css('[class="button"]')).click();
+
+		var list = element.all(by.repeater('contact in vm.contacts'));
+		expect(list.count()).toEqual(4)
 	});
 });
