bfc170fec2e5c86a577c8e8223e606dc3146d0b0
Clare <clare.e.horton@gmail.com>
2017-02-11 01:46:25 -0600

    Done.

--- spec/Index.spec.js
+++ spec/Index.spec.js
@@ -3,5 +3,12 @@ var path = require('path');
 describe('angularjs homepage todo list', function() {
 	it('should add a new contact', function() {
 		browser.get('http://localhost:8080');
+		element(by.css('[value="add"]')).click();
+		var names = element.all(by.model('contact.name'));
+		var phones = element.all(by.model('contact.phone'));
+
+		expect(names.count()).toEqual(4);
+		expect(names.get(1).getAttribute('value')).toEqual('Tim');
+		expect(phones.get(1).getAttribute('value')).toEqual('3934203242');
 	});
 });
